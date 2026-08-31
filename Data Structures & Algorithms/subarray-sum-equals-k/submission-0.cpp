88ada83d6dc6fef5d7b5ac59262b9a2fd9e5a3c9
AmanKr420 <amankrao0@gmail.com>
2026-08-31 11:01:19 +0530

    Add: subarray-sum-equals-k - submission-0

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+
+        unordered_map<int, int> mp;
+
+        // Prefix sum 0 has occurred once.
+        mp[0] = 1;
+
+        int prefixSum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+
+            prefixSum += num;
+
+            // We need an earlier prefix sum of prefixSum - k.
+            if (mp.find(prefixSum - k) != mp.end()) {
+                count += mp[prefixSum - k];
+            }
+
+            // Store frequency of this prefix sum.
+            mp[prefixSum]++;
+        }
+
+        return count;
+    }
+};
